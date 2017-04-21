15b43310b7dab650cf76fc0ecb06fa56aa297394
Aaron Sly <hello@aaronsly.com>
2017-04-21 16:51:53 +0100

    off line Notifications finished

--- assets/js/background.js
+++ assets/js/background.js
@@ -1,6 +1,7 @@
 // Listen for message from content.js and create notification.
 chrome.runtime.onMessage.addListener(function(notification,sender,sendResponse){	
 	sendResponse({responseStatus: "Talk Status: "+notification.status});
+		//Create Notification
 		var opt = {
 		  type: "basic",
 		  title: notification.notifyTitle,
@@ -8,7 +9,16 @@ chrome.runtime.onMessage.addListener(function(notification,sender,sendResponse){
 		  iconUrl: notification.iconUrl,
 		  requireInteraction: true
 		};
-		chrome.notifications.create(notification.notifyId, opt);
 
+		chrome.notifications.create(notification.notifyId, opt);	
+		//console.log(sender);	
+		// Notification on click activates and highlights chrome tab
+		chrome.notifications.onClicked.addListener(function(id) {
+		  chrome.tabs.update(sender.tab.id,{"active":true,"highlighted":true},function (tab){
+			  //console.log("Completed updating tab .." + JSON.stringify(tab));
+			});
+
+		  var clearNotification = function() {chrome.notifications.clear(id)}
+		  setTimeout(clearNotification,750);
+		});
 });
-
